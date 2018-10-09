0a3b6bac670bb82568a43da3552b09ee6a4d43e3
lexman <android@lexman.org>
2018-10-09 22:30:25 +0200

    Still renaming the App

--- app/src/androidTest/java/net/lexman/teddyphoto/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/net/lexman/teddyphoto/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("net.lexman.partagephoto", appContext.getPackageName());
+        assertEquals("net.lexman.teddyphoto", appContext.getPackageName());
     }
 }
